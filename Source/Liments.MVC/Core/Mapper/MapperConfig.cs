6675bf488690b63460821882c6e3c0f58eacf56d
Maksym Borukh <maksboruh@gmail.com>
2020-03-09 18:11:34 +0200

    add mapper

---
+++ Source/Liments.MVC/Core/Mapper/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Liments.MVC.Core.Entities;
+using Liments.MVC.Models;
+
+namespace Liments.MVC.Core.Mapper
+{
+    public static class MapperConfig
+    {
+        public static IMapper CreateMapper()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserViewModel>();
+                cfg.CreateMap<UserViewModel, User>();
+                cfg.CreateMap<Post, PostViewModel>();
+                cfg.CreateMap<PostViewModel, Post>();
+            }).CreateMapper();
+        }
+    }
+}
