8155b92ffdf6f0292ce89df6054f3425346ae45a
Maksym Borukh <maksboruh@gmail.com>
2020-03-09 18:19:18 +0200

    add models

---
+++ Source/Liments.MVC/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Liments.MVC.Models
+{
+    public class RegisterViewModel
+    {
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Incorerct password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
