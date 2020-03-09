0d8d9b77c00a189be0ce8b63304094214aaace39
Maksym Borukh <maksboruh@gmail.com>
2020-03-09 18:12:36 +0200

    add account controler

---
+++ Source/Liments.MVC/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using Liments.MVC.Interfaces;
+using Liments.MVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Liments.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool hasAccess = await _userService.CheckCredentials(model.Email, model.Password);
+
+                if (hasAccess)
+                {
+                    await Authenticate(model.Email);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Incorrect email or password");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isUserExists = await _userService.IsUserExist(model.Email);
+                if (!isUserExists)
+                {
+
+                    if (model.Password == model.ConfirmPassword)
+                    {
+                        var user = new UserViewModel()
+                        {
+                            FirstName = string.Empty,
+                            LastName = string.Empty,
+                            Email = model.Email,
+                            Login = string.Empty,
+                            Password = model.Password,
+                            AccSubs = new List<string>(),
+                            AccFol = new List<string>(),
+                            PndSubs = new List<string>(),
+                            PndFol = new List<string>()
+                        };
+                        await _userService.CreateAsync(user);
+                    }
+
+                    await Authenticate(model.Email);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Incorrect email or password");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string login)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, login)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
