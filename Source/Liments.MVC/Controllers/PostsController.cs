724c245b66e9c170996cc6b3b04fb2f04a2c0ffa
Maksym Borukh <maksboruh@gmail.com>
2020-03-16 14:54:36 +0200

    add PostController

---
+++ Source/Liments.MVC/Controllers/PostsController.cs
@@ -0,0 +1,34 @@
+﻿using Liments.MVC.Interfaces;
+using Liments.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Liments.MVC.Controllers
+{
+    public class PostsController : Controller
+    {
+        private IPostService _postService;
+
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync()
+        {
+            IEnumerable<PostViewModel> posts = await _postService.GetAllPublicAsync();
+            return View(posts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Like(string id)
+        {
+            //TODO check if possible to make async
+            _postService.Like(User.Identity.Name, id);
+            var posts = await _postService.GetAllPublicAsync();
+            return PartialView("_Posts", posts);
+        }
+    }
+}
