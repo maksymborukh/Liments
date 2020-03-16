96fcb427b4cf12c8589d882c58819cbb08064695
Maksym Borukh <maksboruh@gmail.com>
2020-03-16 14:52:17 +0200

    modify

--- Source/Liments.MVC/Controllers/HomeController.cs
+++ Source/Liments.MVC/Controllers/HomeController.cs
@@ -1,30 +1,20 @@
-﻿using Liments.MVC.Interfaces;
-using Liments.MVC.Models;
+﻿using Liments.MVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace Liments.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private IUserService _userService;
 
-        public HomeController(ILogger<HomeController> logger, IUserService service)
-        {
-            _logger = logger;
-            _userService = service;
-        }
-
+        [HttpGet]
         [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
